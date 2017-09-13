94c805ed3f9e62396b3135ab9a65f96318e147d0
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-09-13 10:58:03 -0400

    Completed Coding Conventions

--- VictorHom1/src/intro/CodingConventions.java
+++ VictorHom1/src/intro/CodingConventions.java
@@ -4,7 +4,7 @@ package intro;
 public class CodingConventions {
 	
 	
-	//fields do not instantiate here 
+	//fields, do not instantiate here 
 	private String name;
 	private String description;
 	
@@ -15,9 +15,22 @@ public class CodingConventions {
 	//therefore the name of a constructor must always match the name of the class
 	*/
 	
-	public CodingConventions(){
+	public CodingConventions(String name, int index){
 		//In Java , fields are instantiated in a constructor
-		name = "Mr. Nockles";
-		description = "is a teacher";
+		
+		//there is a local variable 'name' and a field name
+		//the field persists, but the local data is destroyed at the end of the method
+		//distinguish the field by using the reserved word "this"
+		
+		this.name = name;
+		//static reference begins with a class name
+		description = IntroMain.DESCRIPTIONS[index];
+	}
+
+	public void doStuff() {
+		//static method call since the System is a class so it is not called on an individual instance but on the class
+		System.out.println(name + description); //use space btwn operations
+		//this is static since the method does not depend on the instance of 12, as all numbers can be square rooted 
+		System.out.print("The square root of 12 is " + Math.sqrt(12));
 	}
 }
--- VictorHom1/src/intro/IntroMain.java
+++ VictorHom1/src/intro/IntroMain.java
@@ -3,11 +3,21 @@ package intro;//this is a package declaration
 //this is a class header(class declaration)
 public class IntroMain {
 
+	//constants are public
+	public static final String[] DESCRIPTIONS = {" is a teacher", 
+			" is a student", 
+			"is a human being", 
+			"is imaginary"};	
+	
 	public static void main(String[] args) {
 		//declaration of a local variable
 		//notice use of the word 'new' 
 		//new must be used to call a constructor
-		CodingConventions conventionsInstance = new CodingConventions();
+		CodingConventions conventionsInstance = new CodingConventions("Mr.Nocles",2);
+		
+		//instance method call as it is called from an individual instance of the object CodingConventions
+		//dependent on the individual instance
+		conventionsInstance.doStuff();
 	}
 
 }
