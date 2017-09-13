b9b52a50a41bef20e7247f5bd15baf8ccd7b7e6d
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-09-13 11:02:36 -0400

    Completed CodingConventions

--- VictorHom1/src/intro/IntroMain.java
+++ VictorHom1/src/intro/IntroMain.java
@@ -6,14 +6,14 @@ public class IntroMain {
 	//constants are public
 	public static final String[] DESCRIPTIONS = {" is a teacher", 
 			" is a student", 
-			"is a human being", 
-			"is imaginary"};	
+			" is a human being", 
+			" is imaginary"};	
 	
 	public static void main(String[] args) {
 		//declaration of a local variable
 		//notice use of the word 'new' 
 		//new must be used to call a constructor
-		CodingConventions conventionsInstance = new CodingConventions("Mr.Nocles",2);
+		CodingConventions conventionsInstance  = new CodingConventions("Mr.Nocles",2);
 		
 		//instance method call as it is called from an individual instance of the object CodingConventions
 		//dependent on the individual instance
