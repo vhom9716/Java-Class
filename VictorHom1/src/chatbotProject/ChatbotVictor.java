0d0d37f5d042073e679eb389029121c236acf4fa
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-10-02 11:45:36 -0400

    Chatbot update

--- VictorHom1/src/chatbotProject/ChatbotVictor.java
+++ VictorHom1/src/chatbotProject/ChatbotVictor.java
@@ -1,17 +1,32 @@
 package chatbotProject;
 
+import java.util.List;
+
 public class ChatbotVictor implements Topic {
+	
+	private String[] questionWords;
+	private String[] verifications;
+	private String[] negations;
+	private String usedQuestions;
 	
 	private String[] keywords;
 	private String goodbyeWord;
-	private String secretWord;
+	private String[] testWords;
+	private List<String> questions;
+	private List<String> answers;
+	private String tempQuestion;
+	private String tempAnswer;
+	private int temperment = 10; //Index for array of happy to angry statements. 0 is happiest, 20 is angriest.
 	private boolean chatting;
 	
 	public ChatbotVictor(){
-		String[] temp = {"food", "entertainment", "internet", "video games"};
-		keywords= temp;
+		String[] temp = {"social studies", "geography", "history"};
+		keywords = temp;
 		goodbyeWord = "bye";
-		secretWord = "pug";
+		String[] temp2 = {"quiz", "test", "assess"};
+		testWords = temp2;
+		String[] temp3 = {"who", "what", "where", "when", "why"};
+		questionWords = temp3;
 	}	
 	
 	public boolean isTriggered(String response) {
@@ -23,7 +38,9 @@ public class ChatbotVictor implements Topic {
 		return false;
 	}
 	public void startChatting(String response) {
-		ChatbotMain.print("Hey! It sounds like you and I have common interets! Let's talk somemore.");
+		ChatbotMain.print("Hey! It looks like you want some help with Social Studies. Let me help you! Say ready when you want me to start asking questions.");
+		String ready = "ready";
+		
 		chatting = true;
 		while (chatting){
 			response = ChatbotMain.getInput();
@@ -31,14 +48,51 @@ public class ChatbotVictor implements Topic {
 				chatting = false;
 				ChatbotMain.chatbot.startTalking();
 			}else {
-				if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
-					ChatbotMain.print("Oh my goodness! You guessed my favorite thing ever. We are friends now.");
-				}else {
-					ChatbotMain.print("Huh. I don't really get you. Tell me something else?");
+				for (int i = 0; i < testWords.length; i++) {
+					if(ChatbotMain.findKeyword(response, testWords[i], 0) >= 0) {
+						ChatbotMain.print("You have questions you want to be quized on? Thats great! Just enter them here. First enter the question.");
+						
+						tempQuestion = ChatbotMain.getInput();
+						while(!isQuestion(tempQuestion)) {
+							ChatbotMain.print("Sorry. That is not a question. Please try again.");
+							tempQuestion = ChatbotMain.getInput();
+							temperment++;
+						}
+						
+						questions.add(tempQuestion);
+						//questions += tempQuestion;
+						ChatbotMain.print("Great! You entered a question. Now enter the answer.");
+						temperment--;
+						String tempAnswer = ChatbotMain.getInput();
+						answers.add(tempAnswer);
+						//answers += tempAnswer;
+					}	
 				}
+			}
+			
+			if(response.equals(ready.toLowerCase())){
+				startQuestioning();
 			}
 		}
 		
+	}
+	
+	public void startQuestioning() {
+		int randNum = (int) Math.floor(Math.random() * questions.size());
+		while(usedQuestions.indexOf( randNum) > -1) {
+			randNum = (int) Math.floor(Math.random() * questions.size());
+		}
+		ChatbotMain.print(questions.get(randNum));;
+		
+	}
+
+	public boolean isQuestion(String question) {
+		for(int i = 0; i < questionWords.length; i++) {
+			if(question.indexOf(questionWords[i]) > -1) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
