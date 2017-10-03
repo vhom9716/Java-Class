53746df6c3ede1874f565ab01ca35426a1a7ab88
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-10-03 11:45:25 -0400

    Update

--- VictorHom1/src/chatbotProject/ChatbotVictor.java
+++ VictorHom1/src/chatbotProject/ChatbotVictor.java
@@ -4,29 +4,44 @@ import java.util.List;
 
 public class ChatbotVictor implements Topic {
 	
-	private String[] questionWords;
+	//Used to determine emotions
 	private String[] verifications;
 	private String[] negations;
+	private int temperment = 5; //Index for array of happy to angry statements. 0 is happiest, 10 is angriest.
+	
+	
+	//Used to facilitate question adding, question asking, and answer validation
+	private String[] questionWords;
 	private String usedQuestions;
+	private List<String> questions;
+	private List<String> answers;
 	
 	private String[] keywords;
 	private String goodbyeWord;
 	private String[] testWords;
-	private List<String> questions;
-	private List<String> answers;
-	private String tempQuestion;
-	private String tempAnswer;
-	private int temperment = 10; //Index for array of happy to angry statements. 0 is happiest, 20 is angriest.
 	private boolean chatting;
 	
 	public ChatbotVictor(){
 		String[] temp = {"social studies", "geography", "history"};
 		keywords = temp;
+		
+		String[] tempVer = {"That's amazing", "Great", "Awesome", "gdf", "fd", "Okay", "Come on", "Is that the best you can do?", "Wow. I honestly can't believe you actually did something right."};
+		verifications = tempVer;
+		String[] tempNeg = {"Come on you can do it!", "You almost got it right!", "Everyone makes mistakes", "gfd", "sdfsd" ,"That is incorrect.", "Are you listening to me?", "Is that the best you can do?", "I thought I was seeing things. But somehow, you did something wrong. AGAIN.", "Seriously? Are you sure you know how to do this?"};
+		negations = tempNeg;
+
 		goodbyeWord = "bye";
+		
 		String[] temp2 = {"quiz", "test", "assess"};
 		testWords = temp2;
 		String[] temp3 = {"who", "what", "where", "when", "why"};
 		questionWords = temp3;
+		
+		usedQuestions = "";
+		String[] tempQue = {"Who is the first president of the United States?"};
+		String[] tempAns = {"George Washington"};
+		turnArrayToList(questions, tempQue);
+		turnArrayToList(answers, tempAns);
 	}	
 	
 	public boolean isTriggered(String response) {
@@ -38,7 +53,7 @@ public class ChatbotVictor implements Topic {
 		return false;
 	}
 	public void startChatting(String response) {
-		ChatbotMain.print("Hey! It looks like you want some help with Social Studies. Let me help you! Say ready when you want me to start asking questions.");
+		ChatbotMain.print("Hey! It looks like you want some help with Social Studies. Say ready when you want me to start asking questions.");
 		String ready = "ready";
 		
 		chatting = true;
@@ -50,27 +65,22 @@ public class ChatbotVictor implements Topic {
 			}else {
 				for (int i = 0; i < testWords.length; i++) {
 					if(ChatbotMain.findKeyword(response, testWords[i], 0) >= 0) {
-						ChatbotMain.print("You have questions you want to be quized on? Thats great! Just enter them here. First enter the question.");
+						ChatbotMain.print("You have questions you want to be quized on?" + getTempermentStatement(true, "Just enter them here. First enter the question."));
 						
-						tempQuestion = ChatbotMain.getInput();
-						while(!isQuestion(tempQuestion)) {
-							ChatbotMain.print("Sorry. That is not a question. Please try again.");
-							tempQuestion = ChatbotMain.getInput();
+						while(!isQuestion(response)) {
+							ChatbotMain.print(getTempermentStatement(false, "That is not a question. Please try again.")); //Make sure that the questions are unique and not repeated
 							temperment++;
 						}
 						
-						questions.add(tempQuestion);
-						//questions += tempQuestion;
-						ChatbotMain.print("Great! You entered a question. Now enter the answer.");
+						questions.add(response);
+						ChatbotMain.print(getTempermentStatement(true, "You entered a question. Now enter the answer."));
 						temperment--;
-						String tempAnswer = ChatbotMain.getInput();
-						answers.add(tempAnswer);
-						//answers += tempAnswer;
+						answers.add(response);
 					}	
 				}
 			}
 			
-			if(response.equals(ready.toLowerCase())){
+			if(response.indexOf(ready.toLowerCase()) > -1){
 				startQuestioning();
 			}
 		}
@@ -78,12 +88,18 @@ public class ChatbotVictor implements Topic {
 	}
 	
 	public void startQuestioning() {
-		int randNum = (int) Math.floor(Math.random() * questions.size());
-		while(usedQuestions.indexOf( randNum) > -1) {
-			randNum = (int) Math.floor(Math.random() * questions.size());
-		}
-		ChatbotMain.print(questions.get(randNum));;
+		String response = ChatbotMain.getInput(); 
+		int randNum = getUnusedQuestionIndex();
 		
+		ChatbotMain.print(questions.get(randNum));
+		usedQuestions += randNum; 
+			
+		while(!(response.indexOf(answers.get(randNum)) > -1)){
+			temperment++;
+			ChatbotMain.print("You are wrong." + getTempermentStatement(false, "Try again.\n") + questions.get(randNum));
+		}
+		ChatbotMain.print(getTempermentStatement(true, "You are correct. Say ready when you want your next question or request questions you want to be quized on."));
+		temperment--;
 	}
 
 	public boolean isQuestion(String question) {
@@ -95,4 +111,30 @@ public class ChatbotVictor implements Topic {
 		return false;
 	}
 	
+	public int getUnusedQuestionIndex() {
+		if(usedQuestions.length() == questions.size()) {
+			usedQuestions = ""; //When all questions have been used, clears string to allow old questions to be reused.
+		}
+		int randNum = (int) Math.floor(Math.random() * questions.size());
+		while(usedQuestions.indexOf(randNum) > -1) {
+			randNum = (int) Math.floor(Math.random() * questions.size());
+		}
+		return randNum;
+	}
+	
+	//Input True gives verifications. Input False gives negations.
+	public String getTempermentStatement(boolean positive, String s) {
+		if(positive) {
+			return verifications[temperment] + s;
+		}
+		return negations[temperment] + s;
+	}
+	
+	//Adds all elements of the array to a list
+	public void turnArrayToList(List<String> l, String[] a) {
+		for(int i = 0; i < a.length; i++) {
+			String n = "Car";
+			l.add(n);
+		}
+	}
 }
