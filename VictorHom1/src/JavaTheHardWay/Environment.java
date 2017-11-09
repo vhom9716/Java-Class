38c24975693b85879da6da4bdfc49f186f546d8b
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-09 10:41:34 -0500

    Start JavaTheHardWay

---
+++ VictorHom1/src/JavaTheHardWay/Environment.java
@@ -0,0 +1,36 @@
+package JavaTheHardWay;
+
+public class Environment {
+
+	
+	private int plants;
+	private int growthRate;
+	
+	public static void main(String[] args) {
+		Environment farm = new Environment(10);
+		System.out.println(farm);
+		farm.growPlants();
+	}
+	
+	public Environment(int plants) {
+		this.plants = plants;
+		this.growthRate = plants/2;
+	}
+	
+	public void growPlants() {
+		plants += growthRate;
+		System.out.println("\nThere are "+plants+" plants");
+	}
+
+	public boolean eatPlants() {
+		if(plants > 0) {
+			plants--;
+			return true;
+		}
+		return false;
+	}
+	
+	public String toString() {
+		return "A vast field";
+	}
+}
