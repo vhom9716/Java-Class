68926f73b9c3b99ecda010aab8f66654f2f47a91
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-15 11:47:14 -0500

    Habitat

---
+++ VictorHom1/src/JavaTheHardWay/Habitat.java
@@ -0,0 +1,62 @@
+package JavaTheHardWay;
+
+public class Habitat extends Environment{
+
+	private Animal[] animals;
+	
+	public Habitat(int livingCapacity) {
+		super(livingCapacity);
+		animals = new Animal[livingCapacity];
+	}
+
+	public static void main(String[] args) {
+		Habitat h = new Habitat(5);
+		int count = 0;
+		while(count < 6) {
+			Animal a = new Animal();
+			h.addAnimal(a);
+			count++;
+		}
+		System.out.println(h);
+	}
+
+	public String toString() {
+		return super.toString()+"\nHabitants:\n"+animalReport();
+	}
+
+	private String animalReport() {
+		String s = "";
+		for(Animal a: animals) {
+			s += a+"\n";
+		}
+		return s;
+	}
+	
+	public Animal[] getAnimals() {
+		return animals;
+	}
+	
+	
+	public void addAnimal(Animal animal) {
+		int i = 0;
+		while(i < animals.length) {
+			if(animals[i] == null) {
+				animals[i] = animal;
+				break;
+			}
+			i++;
+		}
+	}
+	
+	
+	//overloaded methods - two methods with the same name(but their parameters are different)
+	public void removeAnimal(int i) {
+		animals[i] = null;
+	}
+	
+	public void removeAnimal(Animal a) {
+		for(int i = 0; i < animals.length; i++) {
+			if(animals[i] == a)animals[i] = null;
+		}
+	}
+}
