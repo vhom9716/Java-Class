759488518f451a5fdd4ee1f801019be20842b48e
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-12-05 10:43:19 -0500

    a

---
+++ VictorHom1/src/search/Fibonacci.java
@@ -0,0 +1,24 @@
+package search;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int count = 20;
+		for(int i = 0; i < count; i++) {
+			System.out.println(fibonacci(i));
+		}
+		
+	}
+	
+	public static int fibonacci(int num) {
+		if(num == 0) {
+			return 0;
+		}else {
+			if(num == 1) {
+				return 1;
+			}else {
+				return fibonacci(num - 1) + fibonacci(num - 2);
+			}
+		}
+	}
+}
